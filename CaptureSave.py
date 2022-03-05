db7d007e5a52d5706c628ed3df2830e5e79fedfc
Eliran Malka <eliranmal@gmail.com>
2022-03-05 19:16:52 +0200

    refactor: encapsulation

--- CaptureSave.py
+++ CaptureSave.py
@@ -3,15 +3,31 @@
 from pathlib import Path
 import FreeCAD
 
-project_path = Path(App.ActiveDocument.FileName)
-image_path = project_path.parent.joinpath(
-  "export", (str(project_path.stem) + '.png')
-)
 
-Gui.activeDocument().activeView().viewIsometric()
-Gui.SendMsgToActiveView("ViewFit")
-Gui.runCommand("Std_ViewZoomOut", 0)
-Gui.activeDocument().activeView().saveImage(
-   str(image_path), 1562, 958, "Current"
-)
-Gui.SendMsgToActiveView("Save")
+def resolve_image_path(suffix = ''):
+  project_path = Path(App.ActiveDocument.FileName)
+  return project_path.parent.joinpath(
+    "export", (str(project_path.stem) + suffix + '.png')
+  )
+
+def prepare_view(view):
+  view.viewIsometric()
+  Gui.SendMsgToActiveView("ViewFit")
+  Gui.runCommand("Std_ViewZoomOut", 0)
+
+def capture_image(view, path):
+  view.saveImage(
+     str(path), 1562, 958, "Current"
+  )
+
+def save_project():
+  Gui.SendMsgToActiveView("Save")
+
+
+active_view = Gui.activeDocument().activeView()
+image_path = resolve_image_path()
+
+prepare_view(active_view)
+capture_image(active_view, image_path)
+
+save_project()
