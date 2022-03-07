0cd554868c1825f11c3f3c7933eb6df1e6444622
eliran <eliran@redhat.com>
2022-03-08 00:24:18 +0200

    feat: add FlipCam macro

---
+++ macros/FlipCam.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import FreeCAD
+
+
+def focus_view():
+  Gui.SendMsgToActiveView("ViewFit")
+  Gui.runCommand("Std_ViewZoomOut", 0)
+
+def flip_cam():
+  view = Gui.activeDocument().activeView()
+  cam = view.getCameraNode()
+  cam_orientation_tuple = cam.orientation.getValue().getValue()
+  cam_orientation_flipped_tuple = (
+    cam_orientation_tuple[1], cam_orientation_tuple[0] * -1, cam_orientation_tuple[3] * -1, cam_orientation_tuple[2]
+  )
+  cam.orientation.setValue(cam_orientation_flipped_tuple)
+
+
+flip_cam()
+focus_view()
